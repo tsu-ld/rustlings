62c9ef3eec215ceedcf63e7eb44d635f20a61c6e
tsu <hi@tsu.ar>
2026-05-03 20:55:02 -0300

    feat: complete 04

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -18,9 +18,9 @@ fn main() {
     // from a different language than your own, try an emoji 😉
     // let your_character = '';
 
-    if your_character.is_alphabetic() {
+    if '😈'.is_alphabetic() {
         println!("Alphabetical!");
-    } else if your_character.is_numeric() {
+    } else if '5'.is_numeric() {
         println!("Numerical!");
     } else {
         println!("Neither alphabetic nor numeric!");
