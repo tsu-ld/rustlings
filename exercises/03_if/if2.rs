8b9dd84e0fbf17d105feb04dd35bb3f4d679136f
tsu <hi@tsu.ar>
2026-05-03 20:18:03 -0300

    feat: complete 03

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,10 +1,12 @@
 // TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
-        "Yummy!"
-    } else {
-        1
+        return "Yummy!";
     }
+    if food == "potato" {
+        return "I guess I can eat that.";
+    }
+    "No thanks!"
 }
 
 fn main() {
