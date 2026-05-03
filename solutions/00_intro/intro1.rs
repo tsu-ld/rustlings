03ce27f8045c13b9287bcff4bef5bf766ce6b373
tsu <hi@tsu.ar>
2026-05-03 19:54:29 -0300

    chore: init project

---
+++ solutions/00_intro/intro1.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Congratulations, you finished the first exercise 🎉
+    // As an introduction to Rustlings, the first exercise only required
+    // entering `n` in the terminal to go to the next exercise.
+}
+
