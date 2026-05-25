2d1d85d3617dcd245913408fa43108f4df40b75a
tsu <hi@tsu.ar>
2026-05-25 20:32:39 -0300

    feat: complete 09

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,21 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut string = String::from(input);
+    string.push_str(" world!");
+
+    string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let string = String::from(input);
+
+    string.replace("cars", "balloons")
 }
 
 fn main() {
